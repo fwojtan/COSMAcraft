8e3e43b559a2c279093b501dad5b74221da3330d
fwojtan <finlaywojtan@hotmail.co.uk>
2021-08-31 16:00:44 +0100

    Added Easter Egg

--- cosmacraft/src/main/java/net/fwojtan/cosmacraft/init/ModItems.java
+++ cosmacraft/src/main/java/net/fwojtan/cosmacraft/init/ModItems.java
@@ -3,6 +3,7 @@ package net.fwojtan.cosmacraft.init;
 import net.fwojtan.cosmacraft.common.CosmaCraftItemGroup;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraftforge.common.ToolType;
 import net.minecraftforge.fml.RegistryObject;
 
 import java.util.function.Supplier;
@@ -14,6 +15,7 @@ public class ModItems {
     public static final RegistryObject<Item> DATA_TOOL = Registration.ITEMS.register("data_tool", () -> new Item(new Item.Properties().tab(Registration.cosmaCraftItemGroup)));
     public static final RegistryObject<Item> DOOR_TOOL = Registration.ITEMS.register("door_tool", () -> new Item(new Item.Properties().tab(Registration.cosmaCraftItemGroup)));
     public static final RegistryObject<Item> CABLE_TOGGLE_TOOL = Registration.ITEMS.register("cable_toggle", () -> new Item(new Item.Properties().tab(Registration.cosmaCraftItemGroup)));
+    public static final RegistryObject<Item> MEMORY_CHIP = Registration.ITEMS.register("memory_chip", () -> new Item(new Item.Properties().tab(Registration.cosmaCraftItemGroup)));
     static void register() {}
 
 }
